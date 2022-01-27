92b36e226c643cea0e8b160fc5c8a3781f980558
krazy-glue <76888095+krazy-glue@users.noreply.github.com>
2022-01-27 16:11:48 -0500

    this add the new domains to the list

--- Powershell/Proxyaddresses-AD.ps1
+++ Powershell/Proxyaddresses-AD.ps1
@@ -1,4 +1,4 @@
-﻿$Domains = "bsq.link","qwb.email","bsq-qwb.com","senco.cloud","senco.farm","fruitsbleues.com","fruitsbleues.ca","bleuet.cloud"
+﻿$Domains = "bsq.link","qwb.email","bsq-qwb.com","senco.cloud","senco.farm","fruitsbleues.com","fruitsbleues.ca","bleuet.cloud","congelation.ca","usinecongelation.ca","usinecongelation.com"
 Get-ADuser -Filter * -properties mail | foreach-object {
     $Proxies = @("SMTP:$($_.mail)")
     $Proxies += foreach ($Domain in $Domains)
