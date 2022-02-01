588334770ae001286c582e99259e01956ac5cf78
Mathieu Lapointe <mathieu@lapointe.tech>
2022-02-01 15:12:10 -0500

    this add a script to modify a ini file for each users of a computer

---
+++ Powershell/INI-file-change.ps1
@@ -0,0 +1,9 @@
+$userfolder = get-childitem c:\users
+
+foreach ($userappdata in $userfolder)
+    {
+        if (Test-Path -path "$userappdata\appdata\roaming\Logi-Trace") {
+            copy-item -path "\\wild-blueberries.com\SYSVOL\wild-blueberries.com\scripts\Azure-ini\Connexion.ini" -Destination "$userappdata\appdata\Roaming\Logi-Trace" -Force
+        }
+    }
+##write-output $userfolder
