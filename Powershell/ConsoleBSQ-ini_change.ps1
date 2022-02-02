ff5e4b7b0cb4ac9c24530a3d3451dc0182eeeed1
Mathieu Lapointe <mathieu@lapointe.tech>
2022-02-02 14:29:22 -0500

    this make the consolebsq script usable

--- Powershell/ConsoleBSQ-ini_change.ps1
+++ Powershell/ConsoleBSQ-ini_change.ps1
@@ -1,4 +1,14 @@
+$sfolder = "c:\Logiciels_BSQ\Console_BSQ"
+$notsfolder = "c:\Logiciel_BSQ\Console_BSQ"
 
-if (Test-Path -path "c:\Logiciels_BSQ\Console_BSQ") {
-       copy-item -path ".\Connexion.ini" -Destination "c:\Logiciels_BSQ\Console_BSQ" -Force
+if (Test-Path -path $sfolder) {
+       copy-item -path ".\Connexion.ini" -Destination $sfolder -Force
+       Write-Output "the file has been changed in $notsfolder"
+        }
+ elseif (Test-Path -path $notsfolder) {
+       copy-item -path ".\Connexion.ini" -Destination $notsfolder -Force  
+       Write-Output "the file has been changed in $notsfolder"
+        }
+ else {
+       write-output "the folder was not found on this computer"
         }
