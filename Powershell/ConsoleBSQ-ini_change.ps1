dd106c0664e52ae8f1d4bb8b2f9ac9fa392abccc
krazy-glue <76888095+krazy-glue@users.noreply.github.com>
2022-02-03 15:25:26 -0500

    this try with the module psini

--- Powershell/ConsoleBSQ-ini_change.ps1
+++ Powershell/ConsoleBSQ-ini_change.ps1
@@ -1,12 +1,32 @@
-install-module -name psini -SkipPublisherCheck -Force -AcceptLicense
-import-module psini
+If (-not(Get-InstalledModule psini -ErrorAction silentlycontinue)) {
+    Write-Host "Module does not exist"
+    install-module -name psini -SkipPublisherCheck -Force -AcceptLicense -Verbose
+  }
+  Else {
+    Write-Host "Module exists"
+    import-module psini 
+  }
+
 
 ###  here the script start
+
+$config = convertfrom-stringdata -stringdata @'
+IP=172.30.2.6
+Service=172.30.2.6
+'@
+$ip = "IP=172.30.2.6"
+$service = "Service=172.30.2.6"
+$string = $ip +","+ $service
 
 $console_install = Get-ChildItem HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | foreach-object { Get-ItemProperty $_.PsPath } | Select-object DisplayName,InstallLocation | Sort-Object Displayname -Descending | where-object displayname -match console_bsq
 
 $installedfolder = $console_install.InstallLocation
 
 foreach ($folder in $installedfolder) {
-    set-inicontent -filepath "$folder\Connexion.ini" -Sections "Serveur" -NameValuePairs "IP=172.30.2.6,Service=172.30.2.6" | out-inifile "$folder\Connexion.ini"
+    $ini = get-inicontent -filepath $folder"Connexion.ini" 
+    write-output $ini["Serveur"]["IP","Service"]
+    $iniset = set-inicontent $ini -Debug -Sections "Serveur" -NameValuePairs $string 
+    #$ini | set-inicontent -Sections "Serveur" -NameValuePairs "Service=172.30.2.6"  
+    write-output $ini["Serveur"]["IP","Service"]
+    Out-IniFile -InputObject $iniset -FilePath $folder"Connexion.ini" -Force
 }
