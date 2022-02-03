ae9d6695c7114813a539478e9fcc4a77b858f0dc
krazy-glue <76888095+krazy-glue@users.noreply.github.com>
2022-02-03 15:39:44 -0500

    this make little change

--- Powershell/ConsoleBSQ-ini_change.ps1
+++ Powershell/ConsoleBSQ-ini_change.ps1
@@ -1205,6 +1205,7 @@ Set-Alias oif Out-IniFile
 
 ###  here the script start
 
+
 $console_install = Get-ChildItem HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | foreach-object { Get-ItemProperty $_.PsPath } | Select-object DisplayName,InstallLocation | Sort-Object Displayname -Descending | where-object displayname -match console_bsq
 
 $installedfolder = $console_install.InstallLocation
@@ -1212,11 +1213,10 @@ $installedfolder = $console_install.InstallLocation
 foreach ($folder in $installedfolder) {
     $ini = get-inicontent -filepath $folder"Connexion.ini" 
     write-output $ini["Serveur"]["IP","Service"]
-    $ini | set-inicontent -Sections 'Serveur' -NameValuePairs 'IP=172.30.2.6,Service=172.30.2.6' 
-    write-output $ini["Serveur"]["IP","Service"]
-    Out-IniFile -InputObject $ini -FilePath $folder"Connexion.ini" -Debug
+    $iniset = set-inicontent $ini -Debug -Sections "Serveur" -NameValuePairs $string 
+    #$ini | set-inicontent -Sections "Serveur" -NameValuePairs "Service=172.30.2.6"  
+            
+    Out-IniFile -InputObject $iniset -FilePath $folder"Connexion.ini" -Force
 }
-
-
 
 #| set-inicontent -Sections 'Serveur' -NameValuePairs 'IP=172.30.2.6,Service=172.30.2.6' 
