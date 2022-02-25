877dbab1c669983b336cb2918ec8f383fa1b9e95
Mathieu <math@lapointe.tech>
2022-02-25 11:31:22 -0500

    this change to a multiple case of files

--- Powershell/INI-file-change.ps1
+++ Powershell/INI-file-change.ps1
@@ -1,11 +1,41 @@
 $userfolder = get-childitem c:\users
+$usine = 
 
-foreach ($userappdata in $userfolder) {
+
+if ($env:usine == SB){
+    foreach ($userappdata in $userfolder) {
         write-output "working on $userappdata"
         if (Test-Path -path "c:\users\$userappdata\appdata\roaming\Logi-Trace") {
-            copy-item -path ".\Connexion.ini" -Destination "c:\users\$userappdata\appdata\Roaming\Logi-Trace\" -Force
+            copy-item -path ".\ConnexionSB.ini" -Destination "c:\users\$userappdata\appdata\Roaming\Logi-Trace\Connexion.ini" -Force
+            write-output "the $userappdata folder has been updated"
+            }
+        }
+    }
+elseif ($env:usine == SF){
+    foreach ($userappdata in $userfolder) {
+        write-output "working on $userappdata"
+        if (Test-Path -path "c:\users\$userappdata\appdata\roaming\Logi-Trace") {
+            copy-item -path ".\ConnexionSF.ini" -Destination "c:\users\$userappdata\appdata\Roaming\Logi-Trace\Connexion.ini" -Force
             write-output "the $userappdata folder has been updated"
         }
     }
-
+}
+elseif ($env:usine == DB){
+    foreach ($userappdata in $userfolder) {
+        write-output "working on $userappdata"
+        if (Test-Path -path "c:\users\$userappdata\appdata\roaming\Logi-Trace") {
+            copy-item -path ".\ConnexionDB.ini" -Destination "c:\users\$userappdata\appdata\Roaming\Logi-Trace\Connexion.ini" -Force
+            write-output "the $userappdata folder has been updated"
+        }
+    }
+}
+elseif ($env:usine == NP){
+    foreach ($userappdata in $userfolder) {
+        write-output "working on $userappdata"
+        if (Test-Path -path "c:\users\$userappdata\appdata\roaming\Logi-Trace") {
+            copy-item -path ".\ConnexionNP.ini" -Destination "c:\users\$userappdata\appdata\Roaming\Logi-Trace\Connexion.ini" -Force
+            write-output "the $userappdata folder has been updated"
+        }
+    }
+}
 write-output "all folder in $userfolder has been updated"
