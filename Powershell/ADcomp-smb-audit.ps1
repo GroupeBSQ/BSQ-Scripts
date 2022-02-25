ec636bc13beba4b9eaf11c67540615544970e3ef
Mathieu <math@lapointe.tech>
2022-02-25 11:41:22 -0500

    this modified the script for working in Datto

--- Powershell/ADcomp-smb-audit.ps1
+++ Powershell/ADcomp-smb-audit.ps1
@@ -1,1 +1,3 @@
 Get-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
+
+Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
--- Powershell/INI-file-change.ps1
+++ Powershell/INI-file-change.ps1
@@ -1,40 +1,40 @@
 $userfolder = get-childitem c:\users
-$usine = 
 
 
-if ($env:usine == SB){
+
+if ($env:usine -eq "SB"){
     foreach ($userappdata in $userfolder) {
         write-output "working on $userappdata"
         if (Test-Path -path "c:\users\$userappdata\appdata\roaming\Logi-Trace") {
             copy-item -path ".\ConnexionSB.ini" -Destination "c:\users\$userappdata\appdata\Roaming\Logi-Trace\Connexion.ini" -Force
-            write-output "the $userappdata folder has been updated"
+            write-output "the $userappdata folder has been updated with $env:usine config"
             }
         }
     }
-elseif ($env:usine == SF){
+elseif ($env:usine -eq "SF"){
     foreach ($userappdata in $userfolder) {
         write-output "working on $userappdata"
         if (Test-Path -path "c:\users\$userappdata\appdata\roaming\Logi-Trace") {
             copy-item -path ".\ConnexionSF.ini" -Destination "c:\users\$userappdata\appdata\Roaming\Logi-Trace\Connexion.ini" -Force
-            write-output "the $userappdata folder has been updated"
+            write-output "the $userappdata folder has been updated with $env:usine config"
         }
     }
 }
-elseif ($env:usine == DB){
+elseif ($env:usine -eq "DB"){
     foreach ($userappdata in $userfolder) {
         write-output "working on $userappdata"
         if (Test-Path -path "c:\users\$userappdata\appdata\roaming\Logi-Trace") {
             copy-item -path ".\ConnexionDB.ini" -Destination "c:\users\$userappdata\appdata\Roaming\Logi-Trace\Connexion.ini" -Force
-            write-output "the $userappdata folder has been updated"
+            write-output "the $userappdata folder has been updated with $env:usine config"
         }
     }
 }
-elseif ($env:usine == NP){
+elseif ($env:usine -eq "NP"){
     foreach ($userappdata in $userfolder) {
         write-output "working on $userappdata"
         if (Test-Path -path "c:\users\$userappdata\appdata\roaming\Logi-Trace") {
             copy-item -path ".\ConnexionNP.ini" -Destination "c:\users\$userappdata\appdata\Roaming\Logi-Trace\Connexion.ini" -Force
-            write-output "the $userappdata folder has been updated"
+            write-output "the $userappdata folder has been updated with $env:usine config"
         }
     }
 }
