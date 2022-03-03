1236b8ed4700db5fef93b24f6f278b08b380cbe4
Mathieu <math@lapointe.tech>
2022-03-03 16:20:43 -0500

    this changed the srcipt to accept entry

--- Powershell/Proxyaddresses-AD.ps1
+++ Powershell/Proxyaddresses-AD.ps1
@@ -1,11 +1,27 @@
-﻿$Domains = "bsq.link","qwb.email","bsq-qwb.com","senco.cloud","senco.farm","fruitsbleues.com","fruitsbleues.ca","bleuet.cloud"
-Get-ADuser -Filter * -properties mail | foreach-object {
-    $Proxies = @("SMTP:$($_.mail)")
-    $Proxies += foreach ($Domain in $Domains)
-    {
-        "smtp:$($_.samaccountname)@$Domain"
-    }
-    $Proxies += "sip:$($_.mail)"
+﻿$Domains = "bsq.link","qwb.email","bsq-qwb.com","senco.cloud","senco.farm","fruitsbleues.com","fruitsbleues.ca","bleuet.cloud","congelation.ca","usinecongelation.ca","usinecongelation.com"
+#$Domains += read-host("is there new domain you want to add to this list:$Domains")
+$username = read-host("which user are we adding the proxies address? * for all")
+if ($username -eq "*"){
+    Get-ADuser -Filter * -properties mail | foreach-object {
+        $Proxies = @("SMTP:$($_.mail)")
+        $Proxies += foreach ($Domain in $Domains)
+        {
+            "smtp:$($_.samaccountname)@$Domain"
+        }
+        $Proxies += "sip:$($_.mail)"
     
-    $_ | Set-ADuser -Replace @{ProxyAddresses = $Proxies}
+        $_ | Set-ADuser -Replace @{ProxyAddresses = $Proxies}
+    }
+}
+else {
+    Get-ADuser -identity $username -properties mail | foreach-object {
+        $Proxies = @("SMTP:$($_.mail)")
+        $Proxies += foreach ($Domain in $Domains)
+        {
+            "smtp:$($_.samaccountname)@$Domain"
+        }
+        $Proxies += "sip:$($_.mail)"
+        
+        $_ | Set-ADuser -Replace @{ProxyAddresses = $Proxies}
+    }
 }
