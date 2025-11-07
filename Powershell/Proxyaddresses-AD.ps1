ab0f7cdcbec34d65e56b704b1e853dd40220ce45
krazy-glue <mathieu@lapointe.tech>
2025-11-07 12:03:22 -0500

    changed some domain and created a better script

--- Powershell/Proxyaddresses-AD.ps1
+++ Powershell/Proxyaddresses-AD.ps1
@@ -1,5 +1,22 @@
-﻿$Domains = "bleuet.cloud","bleuets.quebec","bleuetsauvage.quebec","bleuetsauvages.com","bleuetsauvages.quebec","bleuetssauvages.quebec","bsq-qwb.com","bsq.link","congelation.ca","qwb.email","usinecongelation.ca","usinecongelation.com","aadds.wild-blueberries.com"
+﻿$Domains = "bleuet.cloud",
+            "bleuets.quebec",
+            "bleuetsauvage.quebec",
+            "bleuetsauvages.com",
+            "bleuetsauvages.quebec",
+            "bleuetssauvages.quebec",
+            "bsq-qwb.com",
+            "bsq.link",
+            "congelation.ca",
+            "qwb.email",
+            "usinecongelation.ca",
+            "usinecongelation.com",
+            "usinedecongelation.com",
+            "wildblueberries.quebec",
+            "aadds.wild-blueberries.com"
+
 #$Domains += read-host("is there new domain you want to add to this list:$Domains")
+write-host "The following domains will be used to create proxy addresses:"
+$Domains | ForEach-Object { Write-Host "- $_" }
 
 $username = read-host("which user are we adding the proxies address? * for all")
 if ($username -eq "*"){
